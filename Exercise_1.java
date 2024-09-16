1ea59ffea48608751fd7eeb29fa0953763a70586
sdhase123 <51932114+sdhase123@users.noreply.github.com>
2024-09-16 12:52:55 -0500

    pre-course 1 complete

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,7 @@
+// Time complexity O(1)
+// Space complexity o(n)
+// top is index of array that will be used to perform stack operations on an array a.
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +12,47 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        return top < 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = 0;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	System.out.println(top + "is top");
+        if (top < MAX) {
+            a[++top] = x;
+            System.out.println(x + "is pushed to stack");
+            return true;
+        } else {
+            System.out.println("Stack Overflow");
+            return false;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        int peekItem = top < 0 ? 0 : a[top];
+        return peekItem;
     } 
 } 
   
