aff8793e675135fea72ad3a63f7c896e0fb2a888
sdhase123 <51932114+sdhase123@users.noreply.github.com>
2024-09-16 13:09:52 -0500

    pre-course 1 complete

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,6 @@
+// Time complexity : o(1) as popping root, pushing data and setting root will take constant time and there is no loop.
+// Space complexity ;o(n) to store n iitems in stack.
+
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -9,6 +12,8 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+		this.data = data;
+		this.next = null;
         } 
     } 
     
@@ -16,11 +21,21 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+	    return root == null;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+	    StackNode sn =  new StackNode(data);
+	    if(this.root == null) {
+		    this.root = sn;
+	    } else {
+		    sn.next = root;
+		    root = sn;
+	    }
+	    System.out.println (sn.data + " is pushed to Stack");
+	    
     } 
   
     public int pop() 
@@ -28,11 +43,21 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+	    if(!isEmpty()) {
+		    int poppedItem = root.data;
+		    root = root.next;
+		    return poppedItem;
+	    } else {
+		    System.out.println ("Stack Underflow");
+		    return 0;
+	    }
+	    
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+	    return isEmpty() ? 0 : this.root.data;
     } 
   
 	//Driver code
